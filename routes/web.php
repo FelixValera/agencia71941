13ec726cd0042e640658b16aa8658a4b98b15a3c
fretear <fretear@gmail.com>
2024-08-17 01:24:57 -0300

    Clase 4 terminada

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Stmt\TryCatch;
 
 /*Route::get('/', function () {
     return view('welcome');
@@ -48,15 +50,106 @@ Route::get('/vista',function()
 });
 
 //agragando la nuevas rutas creada en clase3
-
 Route::view('/','dashboard');
+
+Route::view('/dashboard', 'dashboard');
+
+Route::get('/region/create',function(){
+
+	return view('regionCreate');
+});
 
 Route::get('/regiones',function(){
 
-	$regiones = DB::select('SELECT * FROM regiones');
+	$regiones = DB::select('SELECT * FROM regiones Order by idRegion');
 
 	return view('regiones',['regiones'=>$regiones]);
 });
+
+Route::get('/destinos',function(){
+
+	$destinos = DB::table('destinos')->orderby('idDestino','asc')->select('idDestino','aeropuerto','precio')->get();
+	
+	return view('destinos',['destinos'=>$destinos]);
+});
+
+Route::post('/region/store',function(){
+
+	$nombre = request()->nombre; //capturar input en laravel
+
+	try{
+
+		DB::table('regiones')->insert(['nombre'=>$nombre]);
+
+		return redirect('/regiones')->with([
+
+			'mensaje'=>'Region: '.$nombre.' Agregado correctamente',
+			'css'=>'green'
+		]); 
+		//se llama flashing devolvemos un mensaje de exito o error 
+		//el metodo "with" lo que hace es que manda una variable de session que luego se elimina
+	}
+	catch(Throwable $th){
+
+		/*es mejor usar esa insterface que "Exception" por que todos las clases que lanzan excepciones implementan la interface Throwable*/
+		return redirect('/regiones')->with([
+
+			'mensaje'=>'No se pudo agregar la region '.$nombre,
+			'css'=>'red'
+		]); 
+	}
+
+});
+
+//en las rutas esto {id} dice que el dato es dinamico 
+Route::get('/region/edit/{id}',function(string $id){
+	//pasamos el parametro de la ruta "id" a la function
+	//obtenemos los datos de la region filtrado por su id 
+
+	$region = DB::table('regiones')->where('idRegion',$id)->first();
+	//en query builder no se requiere colocar el signo "="
+	//get() "trae todos los registros igual a fetchall()" first()"solo trae un registro igual a fetch"  
+	
+	return view('regionEdit',['region'=>$region]);
+});
+
+Route::post('/region/update',function(){
+
+	$nombre = request()->nombre;
+	$idRegion = request()->idRegion;
+	
+	try{
+
+		DB::table('regiones')->where('idRegion',$idRegion)->update(['nombre'=>$nombre]);
+
+		return redirect('/regiones')->with([
+			'mensaje'=>'Actualizado correctamente :)',
+			'css'=>'green'
+		]);
+
+	}
+	catch(Throwable $th){
+		
+		return redirect('/regiones')->with([
+			'mensaje'=>'No se pudo actualizar :(',
+			'css'=>'red'
+		]);
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
